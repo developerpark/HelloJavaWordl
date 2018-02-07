041b8229c12e2535006ef3fe90ff893bda537345
5331 <5331@5331DT.mshome.net>
2018-02-07 17:01:59 +0900

    local change

--- HelloWorld/src/com/srccodes/example/HelloWorld.java
+++ HelloWorld/src/com/srccodes/example/HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		// add comment
+		// add comment.
+		// edit local
 	}	
 
 }
